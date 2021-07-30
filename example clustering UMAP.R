89cc6b1f1e651780b8427b9ebc84195cd5438557
anjabrathenkristoffersen <abrk@fhi.no>
2021-07-30 13:25:23 +0200

    included example of kmean clustering

--- example clustering UMAP.R
+++ example clustering UMAP.R
@@ -37,8 +37,8 @@ params$desc[grepl("CD56", params$desc)]
 VarNavn9 <- params$name[grepl("CD56", params$desc)][1]  #NB pass på å velge riktig....
 params$desc[grepl("CD16", params$desc)]
 VarNavn10 <- params$name[grepl("CD16", params$desc)][1]  #NB pass på å velge riktig....
-params$desc[grepl("CD14", params$desc)]
-VarNavn11 <- params$name[grepl("CD14", params$desc)][1]  #NB pass på å velge riktig....
+params$desc[grepl("CD15", params$desc)]
+VarNavn11 <- params$name[grepl("CD15", params$desc)][1]  #NB pass på å velge riktig....
 params$desc[grepl("CD11c", params$desc)] #denne er ikke med
 VarNavn12 <- params$name[grepl("CD11c", params$desc)][1]  #NB pass på å velge riktig....
 params$desc[grepl("HLA-DR", params$desc)] #denne er ikke med
@@ -58,43 +58,58 @@ arcsinhexprData <- arc_sinh_transform_selected_channels(fcs_data = fcs_data, cha
                                                                                           VarNavn13))
 
 for(i in 1:length(arcsinhexprData)){
-  colnames(arcsinhexprData[[i]]) <- c("CD3", "CD19", "CD4", "CCR7", "CD45RA", "CD8", "CD25", "CD127", "CD56", "CD16", "CD14", "CD11c", "HLA-DR", "Time")
+  colnames(arcsinhexprData[[i]]) <- c("CD3", "CD19", "CD4", "CCR7", "CD45RA", "CD8", "CD25", "CD127", "CD56", "CD16", "CD15", "CD11c", "HLA-DR", "Time")
 }
 
 
 
 arcsinhexprDataMatrix <- list_to_matrix(data = arcsinhexprData, file_names = file_names)
 
-
+# data here only include those column used for clustering (not time and dataset, could also exclude more)
 data <- as.matrix(arcsinhexprDataMatrix[,1:(ncol(arcsinhexprDataMatrix) -2)])
+#clustering using FlowSOM.
 out <- FlowSOM::ReadInput(as.matrix(data), transform = F, scale = F)
 out <- FlowSOM::BuildSOM(out, colsToUse = 1:(ncol(data)))
 out <- FlowSOM::BuildMST(out)
-clusters_pre <- out$map$mapping[, 1]
-#Modified k for each data set,
-outk14 <-  FlowSOM::metaClustering_consensus(out$map$codes, k = 14, seed = 1) 
-clusters_14 <-outk14[clusters_pre]
+cluster_FlowSOM_pre <- out$map$mapping[, 1]
+#Modified k for each data set, try differnt k?
+outk15 <-  FlowSOM::metaClustering_consensus(out$map$codes, k = 15, seed = 1) 
+cluster_FlowSOM_15 <-outk15[cluster_FlowSOM_pre]
 
 #Modified k for each data set,
 outk28 <-  FlowSOM::metaClustering_consensus(out$map$codes, k = 28, seed = 1) 
-clusters_28 <-outk28[clusters_pre]
+cluster_FlowSOM_28 <-outk28[cluster_FlowSOM_pre]
+# FlowSOM do one clustering of 100 clusters, then it is grouped down to wished number of clusters k.
+# by tabulate two of these we see which clusters that are grouped further to get fewer clusters
+table(cluster_FlowSOM_15, cluster_FlowSOM_28)
 
+
+
+
+#clustering with k-means
+k_mean_clustering_different_ks(data, k =c(1,3,5,10,15,20))
+# choose the number of clusters where the prop_within fail to decrease further. 
+Cluster_kmean15 <- kmeans(data, centers = 15)$cluster
+
+#compare how two clustering methods differs
+table(Cluster_kmean15, cluster_FlowSOM_15)
+table(Cluster_kmean15, cluster_FlowSOM_28)
 
 
 #umapAshinfac5 <- umap::umap(data) # take to much time if all data are plottet.
-#Choose to plot only N observations per data set, and repeat 3 times to see differences.
+#Choose to plot only N observations per data set, and repeat 3 times with different seeding to see differences.
 
 set.seed(100) # to ensure same plot every time
 random_cells_for_plotting <- random_cells_vector(arcsinhexprDataMatrix$dataset)
 umapAshinfac5 <- umap::umap(data[random_cells_for_plotting,])
 
-plot(umapAshinfac5$layout, col = col25[clusters_14[random_cells_for_plotting]])
-plot(umapAshinfac5$layout, col = col25[clusters_14[random_cells_for_plotting]], xlim = c(-10,10), ylim = c(-15,10))
-plot(umapAshinfac5$layout, col = col25[clusters_28[random_cells_for_plotting]], xlim = c(-10,10), ylim = c(-15,10))
+plot(umapAshinfac5$layout, col = col25[cluster_FlowSOM_15[random_cells_for_plotting]])
+plot(umapAshinfac5$layout, col = col25[cluster_FlowSOM_15[random_cells_for_plotting]], xlim = c(-10,10), ylim = c(-14,10))
+plot(umapAshinfac5$layout, col = col25[cluster_FlowSOM_28[random_cells_for_plotting]], xlim = c(-10,10), ylim = c(-14,10))
 
 
 
-density_plot_per_cluster(data = data, cluster_per_cell = clusters_14, rand_cells = random_cells_for_plotting)
-#density_plot_per_cluster(data = data, cluster_per_cell = clusters_14, rand_cells = random_cells_for_plotting, plot_cluster = c(1,2))
+density_plot_per_cluster(data = data, cluster_per_cell = cluster_FlowSOM_15, rand_cells = random_cells_for_plotting)
+#density_plot_per_cluster(data = data, cluster_per_cell = cluster_FlowSOM_15, rand_cells = random_cells_for_plotting, plot_cluster = c(1,2))
 
-barplot_per_sample(file_names_per_cell = arcsinhexprDataMatrix$dataset,  cluster_per_cell = clusters_14, rand_cells = random_cells_for_plotting)
+barplot_per_sample(file_names_per_cell = arcsinhexprDataMatrix$dataset,  cluster_per_cell = cluster_FlowSOM_15, rand_cells = random_cells_for_plotting)
