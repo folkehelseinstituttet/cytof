03b1cb026c09d5ae6d5b35b87d2bfad1b02d8a47
anjabrathenkristoffersen <abrk@fhi.no>
2021-08-03 11:42:19 +0200

    included function read_some_data_from_folder()

--- read_data_functions.R
+++ read_data_functions.R
@@ -16,6 +16,26 @@ read_data_from_folder <- function(data_path){
 }
 
 
+#' Read some fcs files in folder
+#' 
+#' @param path a path to the folder with fcs files
+#' @return fcs_data a set of all data produced. 
+
+read_some_data_from_folder <- function(data_path, file_number = 1:4){
+  fcs_files <- fs::path(data_path, rownames(file.info(list.files(data_path))))
+  files_to_open <- basename(fcs_files)
+  files_to_open <- files_to_open[grepl(".fcs", files_to_open)]
+  files_to_open <- files_to_open[file_number]
+  setwd(dirname(fcs_files[1]))
+  file_names <- gsub(".fcs", "", files_to_open)
+  # Read the files into a flowset
+  fcs_data <- flowCore::read.flowSet(files_to_open, transformation = FALSE,
+                                     truncate_max_range = FALSE)
+  return(list(fcs_data = fcs_data, file_names = file_names))
+}
+
+
+
 #' get all parameters in fcs_data with info
 #' 
 #' @param x one file in the fcs_data, e.g. fcs_data[[1]]
