2139cd95f1737e4e43a8b94b2729293fceed4f14
anjabrathenkristoffersen <abrk@fhi.no>
2022-05-19 12:27:29 +0200

    more runs

--- runResults_Regression_Clusters.R
+++ runResults_Regression_Clusters.R
@@ -182,7 +182,7 @@ rmarkdown::render(fs:::path(scriptPath, "resultater_Regression_clusters_manuel.R
 
 scriptPath <- fs::path("H:", "git", "cytof")
 params <- list()
-params$utSti <- fs::path("F:", "Forskningsprosjekter", "PDB 2794 - Immune responses aga_", "Forskningsfiler", "JOBO", "CyTOF", "Analyse i R OUS", "CleanUpGatingMarch2022", "gating_results_Panel2_mars2022", "posNeg", "surrface")
+params$utSti <- fs::path("F:", "Forskningsprosjekter", "PDB 2794 - Immune responses aga_", "Forskningsfiler", "JOBO", "CyTOF", "Analyse i R OUS", "CleanUpGatingMarch2022", "gating_results_Panel2_mars2022", "posNeg", "Stim")
 params$fil <- "Prosent_Gating_Stim_Panel2.csv"
 params$selcected_name <- "stim"
 params$panel <- 2
@@ -202,4 +202,76 @@ params$selcected_name <- "surrface"
 params$panel <- 2
 
 rmarkdown::render(fs:::path(scriptPath, "resultater_Regression_clusters_fraFlowSOM.Rmd"), output_file = fs::path(params$utSti, paste0("Result_", params$fil, ".docx")))
+
+
+
+scriptPath <- fs::path("H:", "git", "cytof")
+params <- list()
+params$utSti <- fs::path("F:", "Forskningsprosjekter", "PDB 2794 - Immune responses aga_", "Forskningsfiler", "JOBO", "CyTOF", "Analyse i R OUS", "CleanUpGatingMarch2022", "gating_results_Panel2_mars2022", "posNeg", "result FlowSOM")
+params$fil <- "Prosent_Gating_klustre_Panel2.csv"
+params$selcected_name <- "surrface"
+params$panel <- 2
+
+rmarkdown::render(fs:::path(scriptPath, "resultater_Regression_clusters_fraFlowSOM.Rmd"), output_file = fs::path(params$utSti, paste0("Result_", params$fil, ".docx")))
+
+
+
+
+scriptPath <- fs::path("H:", "git", "cytof")
+params <- list()
+params$utSti <- fs::path("F:", "Forskningsprosjekter", "PDB 2794 - Immune responses aga_", "Forskningsfiler", "JOBO", "CyTOF", "Analyse i R OUS", "CleanUpGatingMarch2022", "gating_results_Panel2_mars2022", "posNeg", "result FlowSOM")
+params$fil <- "Prosent_Gating_klustre_Panel2_Kopi av sjekk disse JB.csv"
+params$selcected_name <- "surrface"
+params$panel <- 2
+
+rmarkdown::render(fs:::path(scriptPath, "resultater_Regression_clusters_fraFlowSOM.Rmd"), output_file = fs::path(params$utSti, paste0("Result_", params$fil, ".docx")))
+
+
+
+scriptPath <- fs::path("H:", "git", "cytof")
+params <- list()
+params$utSti <- fs::path("F:", "Forskningsprosjekter", "PDB 2794 - Immune responses aga_", "Forskningsfiler", "JOBO", "CyTOF", "Analyse i R OUS", "CleanUpGatingMarch2022", "gating_results_Panel2_mars2022", "posNeg", "result FlowSOM")
+params$fil <- "Prosent_Gating_klustre_Panel2_CD8.csv"
+params$selcected_name <- "surrface"
+params$panel <- 2
+
+rmarkdown::render(fs:::path(scriptPath, "resultater_Regression_clusters_fraFlowSOM.Rmd"), output_file = fs::path(params$utSti, paste0("Result_", params$fil, ".docx")))
+
+
+
+
+
+
+
+
+
+
+#panel 2 1 og 2
+
+
+
+
+scriptPath <- fs::path("H:", "git", "cytof")
+params <- list()
+params$utSti <- fs::path("F:", "Forskningsprosjekter", "PDB 2794 - Immune responses aga_", "Forskningsfiler", "JOBO", "CyTOF", "Analyse i R OUS", "Resultat_Panel2_ALLE_CD8_1og2", "TigtherCleanUp")
+
+params$seed <- 9143 #nb må endres vil man vil gjøre et annet uttrekk
+params$muligeK <- c(15, 20, 25, 30, 35, 40)
+params$selcected_name <- "CD3CD45CD8"
+params$panel <- 2
+params$adj_p <- 0.1
+params$adj_p_methods <- "fdr" # see help(p.adjust) for other methods
+
+rmarkdown::render(fs:::path(scriptPath, "resultater_Regression_clusters.Rmd"), output_file = fs::path(params$utSti, paste0("Result_", params$seed, "_V2.docx")))
+
+
+
+params$seed <- 2390 #nb må endres vil man vil gjøre et annet uttrekk
+params$muligeK <- c(15, 20, 25, 30, 35, 40)
+params$selcected_name <- "CD3CD45CD8"
+params$panel <- 2
+params$adj_p <- 0.1
+params$adj_p_methods <- "fdr" # see help(p.adjust) for other methods
+
+rmarkdown::render(fs:::path(scriptPath, "resultater_Regression_clusters.Rmd"), output_file = fs::path(params$utSti, paste0("Result_", params$seed, "_V2.docx")))
 
